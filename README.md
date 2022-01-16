253a272a75ff79bc334660037dc61a60bcb65c59
nilesh1935 <nilesh.kulk1234@gmail.com>
2022-01-16 16:12:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kqhts1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kqhts1)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Nilesh</h1>
+      <p>Now lets start coding bro</p>
+    </div>
+  );
+}
